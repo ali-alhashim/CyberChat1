df72ec0419f0ce5e49f2e847b24d339ce628a726
ALI MUSA ALHASHIM <93006102+ali-alhashim@users.noreply.github.com>
2022-01-07 12:23:03 +0300

    Camera Intent

--- app/src/main/java/com/example/cyberchat1/fragment/ProfileFragment.kt
+++ app/src/main/java/com/example/cyberchat1/fragment/ProfileFragment.kt
@@ -1,17 +1,23 @@
 package com.example.cyberchat1.fragment
 
+import android.content.ContentValues.TAG
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import com.example.cyberchat1.R
 
 
 
 class ProfileFragment : Fragment() {
 
-
+    lateinit var user_profile_image : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -29,7 +35,25 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+         user_profile_image  = view.findViewById(R.id.user_profile_image)
+
+        user_profile_image.setOnClickListener{
+            Log.d(TAG,"open Camera")
+
+            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            requireActivity().startActivityForResult(cameraIntent, 1888)
+
+        }
     }
 
 
-}
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1888) {
+            val photo: Bitmap = data?.extras?.get("data") as Bitmap
+            user_profile_image.setImageBitmap(photo)
+        }
+    }
+
+
+    }
