310614358150c846a2fef8761c776284ca86020c
ALI MUSA ALHASHIM <93006102+ali-alhashim@users.noreply.github.com>
2022-01-07 12:07:34 +0300

    add profile fragment

--- app/src/main/java/com/example/cyberchat1/activity/MainActivity.kt
+++ app/src/main/java/com/example/cyberchat1/activity/MainActivity.kt
@@ -123,6 +123,36 @@ class MainActivity : AppCompatActivity() {
 
 
 
+        //--------------------Request Permission for CAMERA
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.CAMERA)
+            != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    android.Manifest.permission.CAMERA)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA),
+                    CONTEXT_INCLUDE_CODE)
+
+
+
+                // REQUEST_CODE is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            // Permission has already been granted
+        }
+        //----------------------------
+
+
+
 
         //-----------------------------------------------------------------------------------------
         // first check if this device is already registered
@@ -230,8 +260,12 @@ class MainActivity : AppCompatActivity() {
         {
             //go to profile fragment
 
+            //Log.d(TAG,supportFragmentManager.findFragmentById(R.id.nav_host_fragment)?.id.toString())
+
 
             navController.navigate(R.id.action_MainFragment_to_profileFragment)
+
+
         }
         return true
     }
